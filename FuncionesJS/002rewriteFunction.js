77dca5692dd2919a90f2991db3fc275dcc549580
neumanv <guillermoanv25@gmail.com>
2022-11-04 18:50:33 +0100

    Comentado

--- FuncionesJS/002rewriteFunction.js
+++ FuncionesJS/002rewriteFunction.js
@@ -1,13 +1,15 @@
 "use strict"
 
-function checkAge(age) {
+function checkAge(age) {     //función ternaria
+
     age > 18 ? true : confirm('¿Tienes permiso de tus padres?');
 }
 
 let edad1 = prompt("Edad:")
 checkAge(edad1);
 
-function checkAge2(age){
+function checkAge2(age){      //función más común
+
     if (age == 18 || age > 18) {
         return true;
       }else{
