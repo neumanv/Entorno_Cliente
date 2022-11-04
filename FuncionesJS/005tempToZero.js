a560d82675d8daae459ef1b15c3f06263a7dd4c6
neumanv <guillermoanv25@gmail.com>
2022-11-04 18:47:32 +0100

    Incompleto

--- FuncionesJS/005tempToZero.js
+++ FuncionesJS/005tempToZero.js
@@ -6,11 +6,27 @@ contador(min, sec);
 
 function contador(m, s){
 
-    while(s > 59){
+    /*for(let i = 0; i < 59; i--){
         m++;
+        s = 0;
     }
+        
+    document.write(m + ":" + s);*/
+
+    let tiempo = 1000;  //1s en ms
+
+    let intervalo = window.setInterval(function() {
+
+    if(s == 0){
+        m -= 1;
+    }
+    document.write( + m + ":" + s +"\n");
+    s--;
+    
+    // compruebo que llegue a 0
+    if (s === -1) {
+        // cancelo en intervalo
+        window.clearInterval(intervalo);
+    }
+    }, tiempo);
 }
-
-
-
-
