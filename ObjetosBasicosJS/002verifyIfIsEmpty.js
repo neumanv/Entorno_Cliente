108341006bed2beeb66e056cc9e7157c9c051658
neumanv <guillermoanv25@gmail.com>
2022-11-07 14:15:48 +0100

    Done!

---
+++ ObjetosBasicosJS/002verifyIfIsEmpty.js
@@ -0,0 +1,14 @@
+"use strict"
+let horario = "";
+alert(isEmpty(horario));
+
+function isEmpty(hor){
+
+    if(hor.length === 0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+
