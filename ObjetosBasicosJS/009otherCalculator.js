90e0615afd2fa3afad65e582892f5c08173d41c5
neumanv <guillermoanv25@gmail.com>
2022-11-09 18:43:29 +0100

    Hecho

--- ObjetosBasicosJS/009otherCalculator.js
+++ ObjetosBasicosJS/009otherCalculator.js
@@ -6,12 +6,10 @@ function Calculator(){
         this.a = +prompt("Primer número:");
         this.b = +prompt("Segundo número:");
     };
-
     this.sum = function(){
         let suma = this.a + this.b;  //he tenido que crear esta variable para que al ver el resultado sea más visual
         return this.a + " + " + this.b + " = " + suma;
     };
-
     this.mul = function(){
         return this.a + " x " + this.b + " = " + this.a * this.b;
     };
