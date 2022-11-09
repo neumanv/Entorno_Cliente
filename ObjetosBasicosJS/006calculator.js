a2b86bf82ca71d01da9d1d79b85a83170f945f3a
neumanv <guillermoanv25@gmail.com>
2022-11-09 17:20:24 +0100

    Hecho

---
+++ ObjetosBasicosJS/006calculator.js
@@ -0,0 +1,19 @@
+"use strict"
+
+let calculator = {
+    read(){
+        this.a = +prompt("Primer número:");
+        this.b = +prompt("Segundo número:");
+    },
+    sum(){
+        let suma = this.a + this.b;  //he tenido que crear esta variable para que al ver el resultado sea más visual
+        return this.a + " + " + this.b + " = " + suma;
+    },
+    mul(){
+        return this.a + " x " + this.b + " = " + this.a * this.b;
+    } 
+};
+
+calculator.read();
+document.write(calculator.sum()+ "<br>");
+document.write(calculator.mul());
