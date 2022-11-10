b08dbf88fc06cfef64036ca1bf9211fe42329159
neumanv <guillermoanv25@gmail.com>
2022-11-10 10:07:00 +0100

    Hecho

---
+++ ObjetosBasicosJS/012testCuriosity.js
@@ -0,0 +1,11 @@
+alert( 9999999999999999 );
+
+/*Se muestra 10000000000000000 por que los literales numéricos con valores absolutos iguales a 2^53 o 
+superiores son demasiado grandes para representarlos de forma precisa como enteros
+
+SOLUCIÓN*/
+
+let bd = BigInt(9999999999999999);
+alert(bd);
+
+/*Con una variable BigInt si lo puede mostrar*/
---
+++ ObjetosBasicosJS/016fixInfinityLoop.js
@@ -0,0 +1,6 @@
+"use strict"
+
+let i = 0;
+while (i != 10) {
+  i += 0.2;
+}
