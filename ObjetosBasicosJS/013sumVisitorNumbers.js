d52e52c578ac454c7c4167898f6d88fc9590013e
neumanv <guillermoanv25@gmail.com>
2022-11-10 12:01:25 +0100

    Done

---
+++ ObjetosBasicosJS/013sumVisitorNumbers.js
@@ -0,0 +1,9 @@
+"use strict"
+
+//he puesto el + delante del prompt para que recoja números
+let num1 = +prompt("Primer número");
+let num2 = +prompt("Segundo número");
+
+let suma = num1 + num2
+
+alert(num1 + " + " + num2 + " = " + suma);
