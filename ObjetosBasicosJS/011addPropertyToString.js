be7e9d90bd395bb7a1059e8454961570eb4daa69
neumanv <guillermoanv25@gmail.com>
2022-11-10 09:33:26 +0100

    Resuelto

---
+++ ObjetosBasicosJS/011addPropertyToString.js
@@ -0,0 +1,6 @@
+/*let str = "Peter";
+str.test = 5;
+alert(str.test);
+
+No se pueden añadir propiedades a los strings. Funciona y muestra el alert
+pero el valor que muestra es undefined. Esto pasa por que str no es un objeto*/
