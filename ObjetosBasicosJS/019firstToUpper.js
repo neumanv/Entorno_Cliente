ba99931dbf251820e57f07975cb35329e5e64e74
neumanv <guillermoanv25@gmail.com>
2022-11-11 17:19:20 +0100

    Hecho

--- ObjetosBasicosJS/019firstToUpper.js
+++ ObjetosBasicosJS/019firstToUpper.js
@@ -2,6 +2,10 @@
 
 let palabra = prompt("Escribe una palabra: ");
 
-function ucFirst(word){
+function ucFirst(pal){
 
+    //Separo la primera posicion y la hago mayúscula, luego la uno al resto
+    return pal[0].toUpperCase() + pal.substring(1);
 }
+
+alert(ucFirst(palabra));
