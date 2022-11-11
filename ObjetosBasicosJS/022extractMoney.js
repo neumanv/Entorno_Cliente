008afab3a62abb08863987ebbba9ddaba55e6462
neumanv <guillermoanv25@gmail.com>
2022-11-11 17:50:00 +0100

    Hecho

--- ObjetosBasicosJS/022extractMoney.js
+++ ObjetosBasicosJS/022extractMoney.js
@@ -1,2 +1,10 @@
 "use strict"
 
+let precio = "$120";
+
+function extractCurrencyValue(str) {
+
+    return str.slice(1);    //simplemente le quito la primera posición al string 
+}
+
+document.write(precio + " = " + extractCurrencyValue(precio));
