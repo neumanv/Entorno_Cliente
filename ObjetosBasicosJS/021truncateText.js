ee07ec56ebcdea0a58c3847d1444dcd2ec8fbdd6
neumanv <guillermoanv25@gmail.com>
2022-11-11 17:45:17 +0100

    Hecho

--- ObjetosBasicosJS/021truncateText.js
+++ ObjetosBasicosJS/021truncateText.js
@@ -6,6 +6,7 @@ let tam = +prompt("Introduce el número donde se cortará:");
 function truncate(str, maxlength) {
 
     return (str.length > maxlength) ?
+    /*si el tamaño del string es mayor que el número se corta la cadena en la cifra introducida*/
     str.slice(0, maxlength - 1) + "..." : str;
 }
 
