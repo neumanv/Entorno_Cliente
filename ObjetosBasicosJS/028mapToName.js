be03567b894a642c9398f96111d67afe398f93e2
neumanv <guillermoanv25@gmail.com>
2022-11-13 14:29:26 +0100

    Hecho

---
+++ ObjetosBasicosJS/028mapToName.js
@@ -0,0 +1,8 @@
+"use strict"
+
+let acdc= { name: "ACDC", age: 25 };
+let metallica = { name: "Metallica", age: 30 };
+let guns= { name: "Guns and Roses", age: 28 };
+let bands = [ acdc, metallica, guns];
+let names = bands.map(item => item.name);
+alert( names ); 
