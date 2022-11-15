7e392447879f81bb6856fd2a8a68539fb8614198
neumanv <guillermoanv25@gmail.com>
2022-11-15 16:17:15 +0100

    Hecho

--- ObjetosBasicosJS/008twoFunctionsOneObject.js
+++ ObjetosBasicosJS/008twoFunctionsOneObject.js
@@ -1,16 +1,21 @@
 "use strict"
 
+let ilerna = {};
+
 function A(){
-    let b = 8;
-    return this.b;
+    
+    return ilerna;
 }
 
 function B(){
-    let b = 8;
-    return this.b;
+    
+    return ilerna;
 }
+
+//se ha creado el mismo objeto para devolver en las 2 funciones
 
 let a = new A();
 let b = new B();
 
-alert( a == b ); 
+alert( a == b );        //es true 
+//alert( a === b );     también sería true
