83a43699d2295a0b1bfe4587c7112cf4265f7fdc
neumanv <guillermoanv25@gmail.com>
2022-11-15 17:35:11 +0100

    Cambios realizados

--- ObjetosBasicosJS/029practiceWithBands.js
+++ ObjetosBasicosJS/029practiceWithBands.js
@@ -8,10 +8,10 @@ var grupos = [
 ];
 
 //filtra los grupos por género obtenido aquellos que son de Heavy Metal.
-
-/*let metal = grupos.map(item => item.genero);
-alert(metal.nombre);*/
+//alert(grupos.filter(grupo => grupo.genero == "Heavy Metal"));     [Metallica, Heavy Metal]
 
 //Busca en los grupos usando “find” el grupo con Nombre “Cold Play”.
+//alert(grupos.find(grupo => grupo.nombre == "Cold Play"));       [Metallica, Heavy Metal]
+
 //¿En qué posición del array se encuenta “Cold Play”?
 alert("Cold Play está en la posición: " + grupos.findIndex(grupos => grupos.nombre == "Cold Play"));
