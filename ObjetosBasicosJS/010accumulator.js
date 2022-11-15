426d8b322860957db2617e4fe7625ffa22d6cb95
neumanv <guillermoanv25@gmail.com>
2022-11-15 16:12:58 +0100

    Cambios realizados

--- ObjetosBasicosJS/010accumulator.js
+++ ObjetosBasicosJS/010accumulator.js
@@ -1,22 +1,14 @@
 "use strict"
 
-function Accumulator(startingValue){
+function Accumulator(startingValue) {
 
-    this.value = startingValue;
-
+    this.value = startingValue;     //valor inicial
+  
     this.read = function() {
 
-        let x = 2
-        let suma = 0;
-
-        while (x == 2){
-
-            this.value = +prompt("Escribe un número:");
-            suma += this.value;
-            x = x + 1;
-        }
-    };
-}
+        this.value += +prompt("Escribe un número:");        //se suman los dos números
+    }
+  }
 
 let accumulator = new Accumulator(1); // valor inicial 1
 accumulator.read(); // agrega el valor introducido por el usuario
